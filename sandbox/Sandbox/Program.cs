fb12a00d165a1fb96864294774c2d45488e7b37c
mwana20 <phillipmwanaweika@gmail.com>
2025-01-10 13:17:35 +0300

    exercise 2

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Sandbox Project.");
+        Console.WriteLine("This is in C#");
+
     }
 }
