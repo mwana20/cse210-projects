3c02073e95a55bca9c852578cac4767b9cb110c7
mwana20 <phillipmwanaweika@gmail.com>
2025-01-20 11:33:20 +0300

    week2

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -6,23 +6,23 @@ class Program
     {
         Job job1 = new Job
         {
-            _jobTitle = "Software Engineer",
-            _company = "Microsoft",
+            _jobTitle = "Cyber Tech",
+            _company = "UPDF",
             _startYear = 2019,
             _endYear = 2022
         };
 
         Job job2 = new Job
         {
-            _jobTitle = "Manager",
-            _company = "Apple",
+            _jobTitle = "Cyber Tech",
+            _company = "Beselfless",
             _startYear = 2022,
             _endYear = 2023
         };
 
         Resume myResume = new Resume
         {
-            _personName = "Owen Kasule"
+            _personName = "Mubbala Phillip Kenneth"
         };
         myResume._jobs.Add(job1);
         myResume._jobs.Add(job2);
