77faebe7b59cccf41888c1bfb6b381693ec68a3e
mwana20 <phillipmwanaweika@gmail.com>
2025-01-27 15:36:49 +0300

    w3

--- week03/ScriptureMemorizer/reference.cs
+++ week03/ScriptureMemorizer/reference.cs
@@ -1,19 +1,34 @@
+using System;
+
 public class Reference
 {
     private string _book;
     private int _chapter;
-    private int _verse;
+    private int _startVerse;
     private int _endVerse;
 
     public Reference(string book, int chapter, int verse)
     {
-        // Initialize book, chapter, and verse
+        _book = book;
+        _chapter = chapter;
+        _startVerse = verse;
+        _endVerse = verse;
     }
 
-    public Reference(string book, int chapter, int verse, int endVerse)
+    public Reference(string book, int chapter, int startVerse, int endVerse)
     {
-        // Initialize book, chapter, verse, and endVerse
+        _book = book;
+        _chapter = chapter;
+        _startVerse = startVerse;
+        _endVerse = endVerse;
     }
 
-    public string GetDisplayText() { return ""; }
+    public string GetDisplayText()
+    {
+        if (_startVerse == _endVerse)
+        {
+            return $"{_book} {_chapter}:{_startVerse}";
+        }
+        return $"{_book} {_chapter}:{_startVerse}-{_endVerse}";
+    }
 }
